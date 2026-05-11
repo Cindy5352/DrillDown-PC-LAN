7580116007642c7814f1eab5ce743c442452859d
Cindy <cindy030228@gmail.com>
2026-05-11 18:20:04 +0800

    Delete distribute/output directory

--- distribute/output/portable/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-set "JAVA_EXE=%~dp0jre\bin\java.exe"
-if not exist "%JAVA_EXE%" set "JAVA_EXE=java"
-"%JAVA_EXE%" -Dfile.encoding=UTF-8 -jar "%~dp0DrillDown.jar"
-endlocal
